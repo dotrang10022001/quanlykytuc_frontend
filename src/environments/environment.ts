a8139dc27a96b444508555f4530ac20608f0fcac
Toan Elnino <toanquocnguyen192@gmail.com>
2023-02-01 00:46:04 +0700

    update api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   hmr: false,
   appConfig: 'appconfig.json',
-  // server_api: 'https://65.108.79.164:7201/api/services/app',
-  server_api: "https://localhost:7200/api/services/app",
+  server_api: 'https://65.108.79.164:7201/api/services/app',
+  // server_api: "https://localhost:7200/api/services/app",
   AWS_KEYS: {
     AWS_ACCESS_KEY_ID: 'AKIARI7TTNNVGS6IHRTE',
     AWS_SECRET_ACCESS_KEY: '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',
