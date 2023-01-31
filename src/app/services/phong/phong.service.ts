c367d3a17c0c9fd095d6a408ec446877ce4ad7fd
Toan Elnino <toanquocnguyen192@gmail.com>
2023-01-31 22:50:08 +0700

    enviroment

--- src/app/services/phong/phong.service.ts
+++ src/app/services/phong/phong.service.ts
@@ -1,32 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Phong } from 'src/app/models/phong.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhongService {
 
-  baseUrl: string="https://65.108.79.164:7201/api/services/app/Room/";
+  baseUrl: string=environment.server_api + "/Room";
 
   constructor(private httpClient: HttpClient) { }
 
   themHoacSuaPhong(p: Phong){
-    return this.httpClient.post<any>(this.baseUrl + "CreateOrUpdateRoom", p);
+    return this.httpClient.post<any>(this.baseUrl + "/CreateOrUpdateRoom", p);
   }
 
   getDanhSachPhong(){
-    return this.httpClient.get<any>(this.baseUrl + "GetRoom");
+    return this.httpClient.get<any>(this.baseUrl + "/GetRoom");
   }
 
   getDSPTheoMaToa(maToa: any){
-    return this.httpClient.get<any>(this.baseUrl + "GetRoom?MaToa=" + maToa);
+    return this.httpClient.get<any>(this.baseUrl + "/GetRoom?MaToa=" + maToa);
   }
   xoaPhong(id: number){
-    return this.httpClient.delete<any>(this.baseUrl + "DeleteRoom?id=" + id);
+    return this.httpClient.delete<any>(this.baseUrl + "/DeleteRoom?id=" + id);
   }
 
   getPhongById(id: number){
-    return this.httpClient.get<any>(this.baseUrl + "GetRoom?Id=" + id);
+    return this.httpClient.get<any>(this.baseUrl + "/GetRoom?Id=" + id);
   }
 }
--- src/app/services/sudungdichvu/sudungdichvu.service.ts
+++ src/app/services/sudungdichvu/sudungdichvu.service.ts
@@ -1,29 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SuDungDichVu } from 'src/app/models/sudungdichvu.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SudungdichvuService {
 
-  baseUrl: string="https://65.108.79.164:7201/api/services/app/SDDV/";
+  baseUrl: string=environment.server_api + "/SDDV";
 
   constructor(private httpClient: HttpClient) { }
 
   themHoacSuaSDDV(sddv: SuDungDichVu){
-    return this.httpClient.post<any>(this.baseUrl + "CreateOrUpdateSDDV", sddv);
+    return this.httpClient.post<any>(this.baseUrl + "/CreateOrUpdateSDDV", sddv);
   }
 
   getDanhSachSDDV(loaiDoiTuong: number){
-    return this.httpClient.get<any>(this.baseUrl + "GetSuDungDichVu?LoaiDoiTuong=" + loaiDoiTuong);
+    return this.httpClient.get<any>(this.baseUrl + "/GetSuDungDichVu?LoaiDoiTuong=" + loaiDoiTuong);
   }
   xoaSDDV(id: number){
-    return this.httpClient.delete<any>(this.baseUrl + "DeleteSDDV?id=" + id);
+    return this.httpClient.delete<any>(this.baseUrl + "/DeleteSDDV?id=" + id);
   }
 
   getSDDVById(id: number){
-    return this.httpClient.get<any>(this.baseUrl + "GetSuDungDichVu?Id=" + id);
+    return this.httpClient.get<any>(this.baseUrl + "/GetSuDungDichVu?Id=" + id);
   }
 
 }
